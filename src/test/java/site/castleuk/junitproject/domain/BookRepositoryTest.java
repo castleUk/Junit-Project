cadeed5ac3d18257280ef17e9e2d6e12a5f9ca38
notebook <ppkk24@naver.com>
2023-01-04 12:07:46 +0900

    BeforeEach 적용

--- src/test/java/site/castleuk/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/castleuk/junitproject/domain/BookRepositoryTest.java
@@ -2,6 +2,9 @@ package site.castleuk.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,6 +14,18 @@ public class BookRepositoryTest {
 
   @Autowired
   private BookRepository bookRepository;
+
+  //@BeforeAll // 테스트 시작전에 한번만 실행
+  @BeforeEach // 각 테스트 시작전에 한번씩 실행
+  public void 데이터준비() {
+    String title = "junit";
+    String author = "castleuk";
+    Book book = Book.builder().title(title).author(author).build();
+    bookRepository.save(book);
+  } // 트랜잭션 종료? -> 말안됨!
+
+  // 가정 1 : 데이터준비 + 1 책등록 (T), 데이터준비 + 2 책목록보기(T)   = 사이즈1
+  // 가정2 : 데이터준비 + 1 책등록 + 데이터준비 + 2 책목록보기...?(T)   = 사이즈2
 
   //1, 책 등록
   @Test
@@ -27,12 +42,36 @@ public class BookRepositoryTest {
     assertEquals(author, bookPS.getAuthor());
 
     System.out.println(bookPS.getTitle());
+  } // 트랜잭션 종료(저장된 데이터를 초기화함)
 
-  }
   //2. 책 목록보기
+  @Test
+  public void 책목록보기_test() {
+    //given
+    String title = "junit";
+    String author = "castleuk";
+
+    //when
+    List<Book> booksPS = bookRepository.findAll();
+
+    System.out.println("사이즈 =======================" + booksPS.size());
+    //then
+    assertEquals(title, booksPS.get(0).getTitle());
+    assertEquals(author, booksPS.get(0).getAuthor());
+  } // 트랜잭션 종료(저장된 데이터를 초기화함)
 
   // 3. 책 한건보기
-
+  @Test
+  public void 책한건보기_test() {
+    //given
+    String title = "junit";
+    String author = "castleuk";
+    //when
+    Book bookPS = bookRepository.findById(1L).get();
+    //then
+    assertEquals(title, bookPS.getTitle());
+    assertEquals(author, bookPS.getAuthor());
+  } // 트랜잭션 종료(저장된 데이터를 초기화함)
   //4. 책 수정
 
   //5. 책 삭제
