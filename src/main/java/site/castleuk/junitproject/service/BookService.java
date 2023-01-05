2e8bb14dba5046bd0b9f7e9c4eb150ad8ec8ece4
notebook <ppkk24@naver.com>
2023-01-05 12:11:18 +0900

    메일보내기 생성

--- src/main/java/site/castleuk/junitproject/service/BookService.java
+++ src/main/java/site/castleuk/junitproject/service/BookService.java
@@ -1,12 +1,14 @@
 package site.castleuk.junitproject.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.castleuk.junitproject.domain.Book;
 import site.castleuk.junitproject.domain.BookRepository;
+import site.castleuk.junitproject.util.MailSender;
 import site.castleuk.junitproject.web.dto.BookRespDto;
 import site.castleuk.junitproject.web.dto.BookSaveReqDto;
 
@@ -15,11 +17,17 @@ import site.castleuk.junitproject.web.dto.BookSaveReqDto;
 public class BookService {
 
   private final BookRepository bookRepository;
+  private final MailSender mailSender;
 
   // 1. 책등록
   @Transactional(rollbackFor = RuntimeException.class)
   public BookRespDto 책등록하기(BookSaveReqDto dto) {
     Book bookPS = bookRepository.save(dto.toEntity());
+    if (bookPS != null) {
+      if (!mailSender.send()) {
+        throw new RuntimeException("메일이 전송되지 않았습니다");
+      }
+    }
     return new BookRespDto().toDto(bookPS);
   }
 
@@ -31,10 +39,34 @@ public class BookService {
       .map(new BookRespDto()::toDto)
       .collect(Collectors.toList());
   }
+
   // 3. 책한건보기
+  public BookRespDto 책한건보기(Long id) {
+    Optional<Book> bookOP = bookRepository.findById(id);
+    if (bookOP.isPresent()) {
+      //찾았다면
+      return new BookRespDto().toDto(bookOP.get());
+    } else {
+      throw new RuntimeException("해당 아이디를 찾을수 없습니다");
+    }
+  }
 
   // 4. 책 삭제
+  @Transactional(rollbackFor = RuntimeException.class)
+  public void 책삭제하기(Long id) {
+    bookRepository.deleteById(id);
+  }
 
   // 5. 책 수정
-
+  @Transactional(rollbackFor = RuntimeException.class)
+  public void 책수정하기(Long id, BookSaveReqDto dto) {
+    Optional<Book> bookOP = bookRepository.findById(id);
+    if (bookOP.isPresent()) {
+      Book bookPS = bookOP.get();
+      bookPS.update(dto.getTitle(), dto.getAuthor());
+    } else {
+      throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+    }
+    //메서드 종료시에 더티체킹(flush)으로 update 합니다.
+  }
 }
---
+++ src/main/java/site/castleuk/junitproject/util/MailSender.java
@@ -0,0 +1,5 @@
+package site.castleuk.junitproject.util;
+
+public interface MailSender {
+  boolean send();
+}
