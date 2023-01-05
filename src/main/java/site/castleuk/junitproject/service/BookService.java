21e5c84c2fb0831f8d8e047c265a05568867fd07
notebook <ppkk24@naver.com>
2023-01-05 11:20:05 +0900

    책등록 서비스 생성

--- src/main/java/site/castleuk/junitproject/service/BookService.java
+++ src/main/java/site/castleuk/junitproject/service/BookService.java
@@ -1,8 +1,31 @@
 package site.castleuk.junitproject.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.castleuk.junitproject.domain.Book;
+import site.castleuk.junitproject.domain.BookRepository;
+import site.castleuk.junitproject.web.dto.BookRespDto;
+import site.castleuk.junitproject.web.dto.BookSaveReqDto;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
-  
+
+  private final BookRepository bookRepository;
+
+  // 1. 책등록
+  @Transactional(rollbackFor = RuntimeException.class)
+  public BookRespDto 책등록하기(BookSaveReqDto dto) {
+    Book bookPS = bookRepository.save(dto.toEntity());
+    return new BookRespDto().toDto(bookPS);
+  }
+  // 2. 책목록보기
+
+  // 3. 책한건보기
+
+  // 4. 책 삭제
+
+  // 5. 책 수정
+
 }
--- src/main/java/site/castleuk/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/castleuk/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,21 @@
 package site.castleuk.junitproject.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.castleuk.junitproject.domain.Book;
+
+@NoArgsConstructor
+@Getter
 public class BookRespDto {
-  
+
+  private Long id;
+  private String title;
+  private String author;
+
+  public BookRespDto toDto(Book bookPS) {
+    this.id = bookPS.getId();
+    this.title = bookPS.getTitle();
+    this.author = bookPS.getAuthor();
+    return this;
+  }
 }
